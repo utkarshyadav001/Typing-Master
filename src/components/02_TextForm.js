59b1e456ddb8bf1c520f3f2209557af34b6ea224
Utkarsh <utkarshyadavup22@gmail.com>
2022-05-12 18:36:03 +0530

    Added random-word and changed manifest file

--- src/components/02_TextForm.js
+++ src/components/02_TextForm.js
@@ -3,7 +3,16 @@ import '../style/textForm.css'
 
 
 export default function TextForm(props) {
-  
+
+  let rma = '555';
+  rma += '8555';
+  console.log(rma)
+
+  const hg = ()=>{
+    const randomWords = require('random-words')
+    console.log(randomWords(5))
+  }
+  hg();
   const handleOnChange = (event)=>{
     // console.log("On Change function runing");
     setText(event.target.value);
@@ -15,30 +24,7 @@ export default function TextForm(props) {
     setText(newText);
     props.showAlert("sucess", "Converted to upperCase!");
   }
-  const handleDownClicked = ()=>{
-    let newText =  text.toLowerCase();
-    // console.log("On upper case function runing" , text, newText);
-    setText(newText);
-    props.showAlert("sucess", "Converted to lowerCase!");
-  }
-  const handleClearText = ()=>{
-    // console.log("Clear all text" , text, newText);
-    setText("");
-    props.showAlert("sucess", "Cleared all Text!");
-  }
-  const handleCopyText = ()=>{
-    let copyText  = document.getElementById("Text_box");
-    navigator.clipboard.writeText(copyText.value);
-    // copyText.select()
-    // console.log("Copy to clipBoard text " , text, newText);
-    props.showAlert("sucess", "Copied to ClipBoard!")
-  }
-  const handleExtraSpaces = ()=>{
-    let newText  = text.split(/[ ]+/);
-    setText(newText.join(" "));
-    // console.log("Remove Extra Spaces " , text, newText);
-    props.showAlert("sucess", "Removed Extra Spaces ")
-  }
+  
 
 
   const [text, setText] = useState("");
@@ -50,10 +36,7 @@ export default function TextForm(props) {
           <h1>{props.formHeading}</h1>
           <textarea name="formText" id="Text_box" cols="90" rows="14"  onChange={handleOnChange} value={text}></textarea>
           <button disabled={ text.length ===0? true : false} className="btn" onClick={handleUpClicked}>Convert to Upper case</button>
-          <button disabled={ text.length ===0? true : false} className="btn" onClick={handleDownClicked}>Convert to Lower case</button>
-          <button disabled={ text.length ===0? true : false} className="btn" onClick={handleClearText}>Clear Text</button>
-          <button disabled={ text.length ===0? true : false} className="btn" onClick={handleCopyText}>Copy to clipboard</button>
-          <button disabled={ text.length ===0? true : false} className="btn" onClick={handleExtraSpaces}>Clear Extra Spaces</button>
+          
           <h2>Your text summary</h2>
           <p>{text.split(" ").filter( (words)=>{return words}).length} words and {text.length} characters</p>
           <p>{0.008*text.split(" ").filter( (words)=>{return words}).length} Min read time</p>
